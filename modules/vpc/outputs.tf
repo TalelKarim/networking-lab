cbd23eeee4c7a9b055c1c03aaa91813a642318d6
CHEBBI Talel <tchebbi.ext@XH7LRXXQ-312d0f.local>
2025-06-07 21:50:56 +0200

    Updates

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,10 +6,10 @@ output "vpc_id" {
 
 output "public_subnets_ids" {
   description = "The list of ids of public subnets in the vpc"
-  value = module.vpc.public_subnets
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets_ids" {
   description = "The list of ids of private subnets in the vpc"
-  value = module.vpc.private_subnets
+  value       = module.vpc.private_subnets
 }
