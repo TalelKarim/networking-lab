64c968283bb3af6d55c26eca0e343dec37569589
CHEBBI Talel <tchebbi.ext@XH7LRXXQ-312d0f.local>
2025-06-08 20:04:27 +0200

    Updates compute module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,12 +4,12 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = var.availability_zones
-  public_subnets  = var.is_public ? var.public_subnets_cidrs : []
-  private_subnets = var.is_private ? var.private_subnets_cidrs : []
-
-  enable_nat_gateway = var.enable_nat_gateway
-  enable_vpn_gateway = var.enable_vpn_gateway
+  azs                     = var.availability_zones
+  public_subnets          = var.is_public ? var.public_subnets_cidrs : []
+  private_subnets         = var.is_private ? var.private_subnets_cidrs : []
+  map_public_ip_on_launch = true
+  enable_nat_gateway      = var.enable_nat_gateway
+  enable_vpn_gateway      = var.enable_vpn_gateway
 
   tags = {
     Terraform   = "true"
