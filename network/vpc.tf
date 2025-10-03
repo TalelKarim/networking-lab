8e837e527cf2181fc0a578b219a13967bbc5e0da
CHEBBI Talel <tchebbi.ext@lmn0360.siege.la.priv>
2025-10-03 16:43:17 +0200

    Fix: intra subnets in each az

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,78 +1,117 @@
 module "vpc_web" {
-  source                     = "../modules/vpc"
-  vpc_name                   = "vpc-web"
-  vpc_cidr                   = "10.0.1.0/24"
-  is_public                  = true
-  is_tgw                     = true
-  availability_zones         = ["eu-west-1a", "eu-west-1b"]
-  public_subnets_cidrs       = ["10.0.1.0/26", "10.0.1.64/26"]
-  intra_subnets_cidrs        = ["10.0.1.128/26"]
-  tgw_destination_cidr_block = ["10.0.2.0/24", "10.0.4.0/24"]
+  source             = "../modules/vpc"
+  vpc_name           = "vpc-web"
+  vpc_cidr           = "10.0.1.0/24"
+  is_public          = true
+  is_private         = true
+  is_tgw             = true
+  availability_zones = ["eu-west-1a", "eu-west-1b"]
+
+  private_subnets_cidrs = [
+    "10.0.1.0/27",   # 1a
+    "10.0.1.32/27",  # 1b
+  ]
+  public_subnets_cidrs = [
+    "10.0.1.64/27",  # 1a
+    "10.0.1.96/27",  # 1b
+  ]
+  intra_subnets_cidrs = [
+    "10.0.1.128/27", # 1a
+    "10.0.1.160/27", # 1b
+  ]
+
   transit_gateway_id         = module.tgw.transit_gateway_id
+  tgw_destination_cidr_block = ["10.0.2.0/24", "10.0.4.0/24"]
   flow_log                   = true
   flow_log_group_name        = "/aws/vpc/vpc-web-flow-logs"
   role                       = "frontend-vpc"
 }
 
+
 module "vpc_app" {
-  source                     = "../modules/vpc"
-  vpc_name                   = "vpc-app"
-  vpc_cidr                   = "10.0.2.0/24"
-  is_public                  = true
-  is_private                 = true
-  is_tgw                     = true
-  availability_zones         = ["eu-west-1a", "eu-west-1b"]
-  public_subnets_cidrs       = ["10.0.2.128/26"]
-  private_subnets_cidrs      = ["10.0.2.0/26", "10.0.2.64/26"]
-  intra_subnets_cidrs        = ["10.0.2.192/26"]
+  source             = "../modules/vpc"
+  vpc_name           = "vpc-app"
+  vpc_cidr           = "10.0.2.0/24"
+  is_public          = true
+  is_private         = true
+  is_tgw             = true
+  availability_zones = ["eu-west-1a", "eu-west-1b"]
+
+  private_subnets_cidrs = [
+    "10.0.2.0/27",   # 1a
+    "10.0.2.32/27",  # 1b
+  ]
+  public_subnets_cidrs = [
+    "10.0.2.64/27",  # 1a
+    "10.0.2.96/27",  # 1b
+  ]
+  intra_subnets_cidrs = [
+    "10.0.2.128/27", # 1a
+    "10.0.2.160/27", # 1b
+  ]
+
   transit_gateway_id         = module.tgw.transit_gateway_id
-  flow_log                   = true
-  flow_log_group_name        = "/aws/vpc/vpc-app-flow-logs"
   tgw_destination_cidr_block = ["10.0.1.0/24", "10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway         = true
+  flow_log                   = true
+  flow_log_group_name        = "/aws/vpc/vpc-app-flow-logs"
   role                       = "Backend-vpc"
 }
 
 
+
 module "vpc_db" {
-  source                     = "../modules/vpc"
-  vpc_name                   = "vpc-db"
-  vpc_cidr                   = "10.0.3.0/24"
-  is_public                  = false
-  is_private                 = true
-  is_tgw                     = true
-  availability_zones         = ["eu-west-1a", "eu-west-1b"]
-  public_subnets_cidrs       = ["10.0.3.128/26"]
-  private_subnets_cidrs      = ["10.0.3.0/26", "10.0.3.64/26"]
+  source             = "../modules/vpc"
+  vpc_name           = "vpc-db"
+  vpc_cidr           = "10.0.3.0/24"
+  is_public          = false
+  is_private         = true
+  is_tgw             = true
+  availability_zones = ["eu-west-1a", "eu-west-1b"]
+
+  private_subnets_cidrs = [
+    "10.0.3.0/27",   # 1a
+    "10.0.3.32/27",  # 1b
+  ]
+  intra_subnets_cidrs = [
+    "10.0.3.64/27",  # 1a
+    "10.0.3.96/27",  # 1b
+  ]
+
   transit_gateway_id         = module.tgw.transit_gateway_id
   tgw_destination_cidr_block = ["10.0.2.0/24", "10.0.4.0/24"]
-  intra_subnets_cidrs        = ["10.0.3.192/26"]
   enable_nat_gateway         = false
-  role                       = "DAtabase-vpc"
+  role                       = "Database-vpc"
 }
+
 
 
 module "vpc_shared" {
-  source                     = "../modules/vpc"
-  vpc_name                   = "vpc-shared"
-  vpc_cidr                   = "10.0.4.0/24"
-  is_public                  = false
-  is_private                 = true
-  is_tgw                     = true
-  availability_zones         = ["eu-west-1a", "eu-west-1b"]
-  public_subnets_cidrs       = [] # pas de sous-réseaux publics directs
-  transit_gateway_id         = module.tgw.transit_gateway_id
-  intra_subnets_cidrs        = ["10.0.4.128/27"]
-  tgw_destination_cidr_block = ["10.0.2.0/24", "10.0.3.0/24", "10.0.1.0/24", "10.0.0.0/24"]
-  private_subnets_cidrs = [
-    "10.0.4.0/27",  # Subnet-Shared-NLB-AZ-A
-    "10.0.4.64/27", # Subnet-Shared-NLB-AZ-B
-    "10.0.4.32/27", # Subnet-Shared-GWLB-AZ-A
-    "10.0.4.96/27"  # Subnet-Shared-GWLB-AZ-B
-  ]
-  role = "Shared-vpc"
+  source             = "../modules/vpc"
+  vpc_name           = "vpc-shared"
+  vpc_cidr           = "10.0.4.0/24"
+  is_public          = false
+  is_private         = true
+  is_tgw             = true
+  availability_zones = ["eu-west-1a", "eu-west-1b"]
 
+  private_subnets_cidrs = [
+    "10.0.4.0/27",   # NLB A (1a)
+    "10.0.4.64/27",  # NLB B (1b)
+    "10.0.4.32/27",  # GWLB A (1a)
+    "10.0.4.96/27",  # GWLB B (1b)
+  ]
+
+  intra_subnets_cidrs = [
+    "10.0.4.128/27", # 1a
+    "10.0.4.160/27", # 1b
+  ]
+
+  transit_gateway_id         = module.tgw.transit_gateway_id
+  tgw_destination_cidr_block = ["10.0.2.0/24", "10.0.3.0/24", "10.0.1.0/24", "10.0.0.0/24"]
+  role                       = "Shared-vpc"
 }
+
 
 
 
