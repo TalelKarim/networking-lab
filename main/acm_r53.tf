22b1975066ef5a3be504ccd19655bdb03c09701e
CHEBBI Talel <tchebbi.ext@XH7LRXXQ-312d0f.local>
2025-10-14 18:04:43 +0200

    Fix : wrong zone id

--- main/acm_r53.tf
+++ main/acm_r53.tf
@@ -13,7 +13,7 @@ resource "aws_route53_record" "web_cert_validation" {
       value = dvo.resource_record_value
     }
   }
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = var.custom_zone_tkc_id
   name    = each.value.name
   type    = each.value.type
   records = [each.value.value]
@@ -28,7 +28,7 @@ resource "aws_acm_certificate_validation" "web" {
 
 
 resource "aws_route53_record" "web_alias" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = var.custom_zone_tkc_id
   name    = "www.talelkarimchebbi.com"
   type    = "A"
   alias {
