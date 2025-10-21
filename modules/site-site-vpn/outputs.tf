66fcae155a2ee7c5bff0377a931b43118fe00bea
CHEBBI Talel <tchebbi.ext@lmn0360.siege.la.priv>
2025-10-21 18:11:38 +0200

    Remove attachement id

--- modules/site-site-vpn/outputs.tf
+++ modules/site-site-vpn/outputs.tf
@@ -21,9 +21,3 @@ output "vpn_tunnel2_psk" {
 output "vpn_attachment_id" {
   value = aws_vpn_connection.onprem_to_tgw.transit_gateway_attachment_id
 }
-
-
-
-output "vpn_attachment_id" {
-  value = aws_vpn_connection.onprem_to_tgw.transit_gateway_attachment_id
-}
