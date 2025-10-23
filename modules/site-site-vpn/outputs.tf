b472f5be62b4f235e3c929a14074ddaed7565626
CHEBBI Talel <tchebbi.ext@XH7LRXXQ-312d0f.local>
2025-10-23 17:15:16 +0200

    Add outputs

--- modules/site-site-vpn/outputs.tf
+++ modules/site-site-vpn/outputs.tf
@@ -1,23 +1,12 @@
-output "vpn_tunnel1_outside_ip" {
-  value = aws_vpn_connection.onprem_to_tgw.tunnel1_address
-}
+output "vpn_id" { value = aws_vpn_connection.onprem_to_tgw.id }
 
-output "vpn_tunnel2_outside_ip" {
-  value = aws_vpn_connection.onprem_to_tgw.tunnel2_address
-}
-
-output "vpn_tunnel1_psk" {
-  value       = aws_vpn_connection.onprem_to_tgw.tunnel1_preshared_key
-  description = "PSK du tunnel 1 (générée par AWS si non fournie)"
-  sensitive   = true
-}
-
-output "vpn_tunnel2_psk" {
-  value       = aws_vpn_connection.onprem_to_tgw.tunnel2_preshared_key
-  description = "PSK du tunnel 2 (générée par AWS si non fournie)"
-  sensitive   = true
-}
-
-output "vpn_attachment_id" {
-  value = aws_vpn_connection.onprem_to_tgw.transit_gateway_attachment_id
-}
+output "tunnel1_outside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel1_address }
+output "tunnel2_outside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel2_address }
+output "tunnel1_psk" { value = aws_vpn_connection.onprem_to_tgw.tunnel1_preshared_key }
+output "tunnel2_psk" { value = aws_vpn_connection.onprem_to_tgw.tunnel2_preshared_key }
+output "tunnel1_inside_cidr" { value = aws_vpn_connection.onprem_to_tgw.tunnel1_inside_cidr } # ex: 169.254.8.8/30
+output "tunnel2_inside_cidr" { value = aws_vpn_connection.onprem_to_tgw.tunnel2_inside_cidr }
+output "tunnel1_cgw_inside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel1_cgw_inside_address }
+output "tunnel1_tgw_inside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel1_vgw_inside_address } # “vgw” = endpoint AWS
+output "tunnel2_cgw_inside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel2_cgw_inside_address }
+output "tunnel2_tgw_inside_ip" { value = aws_vpn_connection.onprem_to_tgw.tunnel2_vgw_inside_address }
